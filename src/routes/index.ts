f35ed06bc4231e5949554b01543fcce13df012d9
Sabuj Islam <sabuj0338@gmail.com>
2025-03-06 20:19:17 +0600

    removed unused files

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,10 @@
 import express from "express";
 import authRoutes from "./auth.routes";
-// import taskRoutes from "./task.routes";
 import userRoutes from "./user.routes";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
-// router.use("/tasks", taskRoutes);
 router.use("/users", userRoutes);
 
 export default router;
